3f396eb2284599edfa4cd4b6461ce11a45639909
Cyril Wei <teakscn@gmail.com>
2014-07-06 17:38:03 +0800

    [cyril] Initial commit; created project.

---
+++ HexaworldCore/HexaworldCore.h
@@ -0,0 +1,19 @@
+//
+//  HexaworldCore.h
+//  HexaworldCore
+//
+//  Created by Cyril Wei on 7/6/14.
+//  Copyright (c) 2014 Cyril Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HexaworldCore.
+FOUNDATION_EXPORT double HexaworldCoreVersionNumber;
+
+//! Project version string for HexaworldCore.
+FOUNDATION_EXPORT const unsigned char HexaworldCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HexaworldCore/PublicHeader.h>
+
+
