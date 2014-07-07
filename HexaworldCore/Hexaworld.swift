efdb946b095194bc690c1c4dbf3ffdcb1316f4dc
Cyril Wei <teakscn@gmail.com>
2014-07-07 19:26:56 +0800

    [cyril] added 1 more cell for odd indexed columns.

--- HexaworldCore/Hexaworld.swift
+++ HexaworldCore/Hexaworld.swift
@@ -12,29 +12,52 @@ class Hexaworld {
     let columns: Int
     let rows: Int
     
-    let cells: HexaworldCell?[]
+    let cells: Array<HexaworldCell?>
     
     subscript(column: Int, row: Int) -> HexaworldCell? {
         get {
-            if row >= rows || row < 0 || column >= columns || column < 0 {
+            let index = cellIndex(column, row: row)
+            if index == -1 {
                 return nil
             }
             
-            return cells[row * columns + column]
+            return cells[index]
         }
         set {
-            cells[row * columns + column] = newValue
+            let index = cellIndex(column, row: row)
+            if index == -1 {
+                return
+            }
+            
+            cells[index] = newValue
         }
     }
-    
+
+    func cellIndex(column: Int, row: Int) -> Int {
+        if column >= columns || column < 0 || row < 0 {
+            return -1
+        }
+        
+        if column % 2 == 0 && row >= rows {
+            return -1
+        }
+        
+        if column % 2 == 1 && row > rows {
+            return -1
+        }
+        
+        return column * rows + row + column / 2
+    }
+
     init(columns: Int, rows: Int) {
         self.columns = columns;
         self.rows = rows;
         
-        cells = []
+        let cellCount = columns * rows + columns / 2
+        cells = Array<HexaworldCell?>(count: cellCount, repeatedValue: nil)
         
-        for i in 0..(columns * rows) {
-            cells.append(HexaworldCell())
+        for i in 0..cellCount {
+            cells[i] = HexaworldCell()
         }
     }
 }
--- HexaworldCoreTests/HexaworldTests.swift
+++ HexaworldCoreTests/HexaworldTests.swift
@@ -12,10 +12,13 @@ import HexaworldCore
 class HexaworldTests: XCTestCase {
     let expectedColumns = 5
     let expectedRows = 3
-    
+
+    var world = Hexaworld(columns: 0, rows: 0)
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        world = Hexaworld(columns: expectedColumns, rows: expectedRows)
     }
     
     override func tearDown() {
@@ -24,18 +27,15 @@ class HexaworldTests: XCTestCase {
     }
 
     func test_should_create_hexaworld_with_width_and_height() {
-        let world = Hexaworld(columns: expectedColumns, rows: expectedRows)
         XCTAssertEqual(expectedColumns, world.columns)
         XCTAssertEqual(expectedRows, world.rows)
     }
     
-    func test_should_have_15_cells_in_5_by_3_world() {
-        let world = Hexaworld(columns: expectedColumns, rows: expectedRows)
-        XCTAssertEqual(15, world.cells.count)
+    func test_should_have_17_cells_in_5_by_3_world() {
+        XCTAssertEqual(17, world.cells.count)
     }
     
     func test_should_return_cell_at_position_0_1() {
-        let world = Hexaworld(columns: expectedColumns, rows: expectedRows)
         let cell = world[0, 1]
         if let realCell = cell? {
             // should be success
@@ -45,8 +45,23 @@ class HexaworldTests: XCTestCase {
     }
     
     func test_should_return_nil_at_illegal_position() {
-        let world = Hexaworld(columns: expectedColumns, rows: expectedRows)
         let cell = world[expectedColumns, expectedRows]
+        if let realCell = cell? {
+            XCTFail()
+        }
+    }
+    
+    func test_odd_indexed_column_should_have_1_more_cell() {
+        let cell = world[1, expectedRows]
+        if let realCell = cell? {
+            // should be success
+        } else {
+            XCTFail()
+        }
+    }
+
+    func test_even_indexed_column_should_have_normal_cell_count() {
+        let cell = world[2, expectedRows]
         if let realCell = cell? {
             XCTFail()
         }
