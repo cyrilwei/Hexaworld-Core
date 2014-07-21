5d7912026527622c492f6c9b00991339b21e7803
Cyril Wei <teakscn@gmail.com>
2014-07-21 21:49:27 +0800

    [cyril] supported move cells down.

---
+++ HexaworldCoreTests/HexaworldMoveTests.swift
@@ -0,0 +1,48 @@
+//
+//  HexaworldMoveTests.swift
+//  HexaworldCore
+//
+//  Created by Cyril Wei on 7/21/14.
+//  Copyright (c) 2014 Cyril Wei. All rights reserved.
+//
+
+import XCTest
+import HexaworldCore
+
+class HexaworldMoveTests: XCTestCase {
+    let expectedColumns = 5
+    let expectedRows = 3
+    
+    let originalCellColumn = 1
+    let originalCellRow = 1
+    
+    var world: Hexaworld!
+    
+    override func setUp() {
+        super.setUp()
+        
+        world = Hexaworld(layout: HexaLayout.createLandscapeLayout(expectedColumns, rows: expectedRows))
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func test_should_move_down_to_fill_empty_cell() {
+        let cellFrom = world[originalCellColumn, originalCellRow]
+        
+        world[originalCellColumn, originalCellRow - 1] = nil
+
+        world.fillHoles(.Down)
+        
+        let cellAfterBeMoved = world[originalCellColumn, originalCellRow - 1]
+        
+        if let realCell = cellAfterBeMoved? {
+            XCTAssertTrue(cellFrom! === realCell)
+        } else {
+            XCTFail()
+        }
+        
+    }
+
+}
